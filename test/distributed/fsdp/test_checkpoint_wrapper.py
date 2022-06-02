4c94f87372361fc2db47c5623801803f28a3a8eb
Rohan Varma <rvarm1@fb.com>
2022-06-02 19:36:20 +0000

    Checkpoint util
    ghstack-source-id: a2e11b710590677e1cae270982c8fd227b236b89
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/78704

--- test/distributed/fsdp/test_checkpoint_wrapper.py
+++ test/distributed/fsdp/test_checkpoint_wrapper.py
@@ -5,7 +5,7 @@ from copy import deepcopy
 import torch
 import torch.nn as nn
 from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import (
-    checkpoint_wrapper,
+    checkpoint_wrapper, apply_activation_checkpointing_wrapper, CheckpointWrapper
 )
 
 from torch.testing._internal.common_utils import (
@@ -39,6 +39,51 @@ class CheckpointWrapperTest(TestCase):
         lin.load_state_dict(state_dict)
         for p1, p2 in zip(lin.parameters(), lin_new.parameters()):
             self.assertEqual(p1, p2)
+
+    def test_apply_activation_checkpointing_wrapper(self):
+        class LinearWithBatchNorm(nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.lin = nn.Linear(10, 10)
+                self.bn = nn.BatchNorm1d(10)
+                self.nested_linear = nn.Sequential(nn.Linear(10, 10))
+
+            def forward(self, x):
+                return self.bn(self.nested_linear(self.lin(x)))
+
+        class MyModel(nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.seq = nn.Sequential(
+                    LinearWithBatchNorm(), LinearWithBatchNorm(), LinearWithBatchNorm()
+                )
+
+            def forward(self, x):
+                return self.seq(x)
+
+        model = MyModel()
+        check_fn = lambda m, l: isinstance(l, nn.Linear)
+        apply_activation_checkpointing_wrapper(
+            model, checkpoint_wrapper_fn=checkpoint_wrapper, check_fn=check_fn
+        )
+        print(model)
+        for j in range(3):
+            self.assertTrue(isinstance(model.seq[j].lin, CheckpointWrapper))
+            self.assertTrue(isinstance(model.seq[j].nested_linear[0], CheckpointWrapper))
+
+        inp = torch.randn(4, 10, requires_grad=True)
+        for i in range(6):
+            loss = model(inp).sum().backward()
+            self.assertTrue(loss.requires_grad)
+            # ensure checkpointed part of model has gradients
+            for j in range(3):
+                weight_lin = model.seq[j].lin.mod.weight
+                bias_lin = model.seq[j].lin.mod.bias
+                weight_nested_lin = model.seq[j].nested_linear[0].mod.weight
+                bias_nested_lin = model.seq[j].nested_linear[0].mod.bias
+                for param in [weight_lin, bias_lin, weight_nested_lin, bias_nested_lin]:
+                    self.assertTrue(param.requires_grad)
+                    self.assertFalse(param.grad is None)
 
 
 if __name__ == "__main__":
